7eea0a5a2ca8ac5c30461ef9fa21e4b2deb54869
Art Todorov <art.todorov@me.com>
2020-03-09 22:39:00 -0700

    fetch & display repos

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor () {
     this.client_id = '4b3303a99085072ed756';
     this.client_secret = '4cca236a6e6ba3812d4ecf7bf68b9cd904c843e7';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
